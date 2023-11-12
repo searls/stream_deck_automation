db96aaa9ff3086fb1cb0d09080f863b28bfbc592
Justin Searls <searls@gmail.com>
2023-11-12 09:26:08 -0500

    get closer

--- raycast/improve-grammar.sh
+++ raycast/improve-grammar.sh
@@ -3,13 +3,20 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Improve Grammar
-# @raycast.mode silent
+# @raycast.mode fullOutput
 
 # Optional parameters:
 # @raycast.icon 👨‍🏫
 # @raycast.packageName GPT Scripts
-# @raycast.argument1 { "type": "text", "placeholder": "なんとか" }
+# @raycast.argument1 { "type": "text", "placeholder": "なんとか", "optional": true }
 
 set -e
 
-transform_text --prompt "できれば、文法を改善してください。出来ない場合は変えなくて繰り返してくれ" <<< "$1"
+# Load Ruby
+eval "$(rbenv init -)"
+# Load API keys
+source ~/icloud-drive/dotfiles/.env
+
+clipboard="$(pbpaste)"
+input="${1:-clipboard}"
+../script/transform_text --prompt "できれば、文法を改善してください。出来ない場合は変えなくて繰り返してくれ" <<< "${1-}"
